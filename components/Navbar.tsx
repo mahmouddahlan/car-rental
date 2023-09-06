0a87bdf8320be17c8795e9720b01e898075a363e
Mahmoud Dahlan <141684019+mahmouddahlan@users.noreply.github.com>
2023-09-06 01:51:17 -0400

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,13 +7,19 @@ const NavBar = () => (
   <header className='w-full  absolute z-10'>
     <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
       <Link href='/' className='flex justify-center items-center'>
-        <h1 className="text-3xl font-extrabold tracking-widest">Car Link</h1>
+      <Image
+          src='/logo3.svg'
+          width={5}
+          height={5}
+          className='absolute w-[50px] h-[50px] ml-40'
+          alt='car model'
+        />
+        <h1 className="text-2xl font-extrabold tracking-widest  sm:text-[24px] text-[30px]">CarLink</h1>
       </Link>
-
       <CustomButton
         title='Sign in'
         btnType='button'
-        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
+        containerStyles="bg-primary-blue text-white rounded-full mt-3"
       />
     </nav>
   </header>
